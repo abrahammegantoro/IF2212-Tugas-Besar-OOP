096ac49481a668d5c7df0c60e28e613feb98abfe
ClaraSinaga <89370019+ClaraSinaga@users.noreply.github.com>
2023-04-26 02:23:12 +0700

    masakan

--- src/Item/Masakan/Masakan.java
+++ src/Item/Masakan/Masakan.java
@@ -1,7 +1,36 @@
 package src.Item.Masakan;
 import src.Item.Item;
 import src.Item.Edible;
+import src.Item.BahanBaku.*;
 
-public class Masakan extends Item implements Edible {
-    
+import java.util.ArrayList;
+
+public abstract class Masakan extends Item implements Edible {
+    protected static ArrayList<BahanBaku> bahanBaku = new ArrayList<>();
+    private int kekenyangan;
+    private int porsi;
+
+    //Konstruktor
+    public Masakan(int porsi, int kekenyangan){
+        this.porsi = porsi;
+        this.kekenyangan = kekenyangan; 
+    }
+
+    // Getter
+    public int getKekenyangan(){
+        return kekenyangan;
+    }
+
+    public int getPorsi(){
+        return porsi;
+    }
+
+    public ArrayList<BahanBaku> getBahanBaku(){
+        return bahanBaku;
+    }
+
+    public float getWaktuMasak(){
+        return (float) (1.5* getKekenyangan());
+    }
+
 }
