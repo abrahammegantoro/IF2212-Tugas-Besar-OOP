e709c26b85e883e71eb13b5c4b1da6eeec648841
ClaraSinaga <89370019+ClaraSinaga@users.noreply.github.com>
2023-04-26 02:35:05 +0700

    bed (tp method tidur msh ga beenr)

--- src/Item/Furniture/Bed/QueenBed.java
+++ src/Item/Furniture/Bed/QueenBed.java
@@ -1,5 +1,9 @@
 package src.Item.Furniture.Bed;
 
+import src.World.Point;
+
 public class QueenBed extends Bed{
-    
+    public QueenBed(Point lokasi){
+        super("Kasur Queen Size", lokasi,  4, 2, 100);
+    }
 }
