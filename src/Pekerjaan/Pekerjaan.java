3e49b26f1f82c26292ddbdb3320214dbb7a231fe
Shulhann <18221051@std.stei.itb.ac.id>
2023-04-27 00:58:01 +0700

    Update Pekerjaan.java

--- src/Pekerjaan/Pekerjaan.java
+++ src/Pekerjaan/Pekerjaan.java
@@ -1,5 +1,13 @@
 package src.Pekerjaan;
 
 public class Pekerjaan {
+    private String namaPekerjaan;
+    private int gaji;
     
+    public Pekerjaan(String namaPekerjaan){
+        this.namaPekerjaan = namaPekerjaan;
+    }
+    public Pekerjaan(int randomnumber){
+        
+    }
 }
