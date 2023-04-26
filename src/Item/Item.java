528957581557d1c60c7f29ff892e6f8726715f54
Shulhann <18221051@std.stei.itb.ac.id>
2023-04-27 00:36:38 +0700

    Update Item.java

--- src/Item/Item.java
+++ src/Item/Item.java
@@ -1,5 +1,10 @@
 package src.Item;
 
 public abstract class Item {
-    
+    private String nama;
+
+    public Item(String nama){
+        this.nama = nama;
+    }
+    public String getNama();
 }
