29550743fa600fb6bcb11eea0d323380e5c1fe77
ClaraSinaga <89370019+ClaraSinaga@users.noreply.github.com>
2023-04-28 11:46:20 +0700

    clock, lihatWaktu

--- src/Item/Furniture/Clock.java
+++ src/Item/Furniture/Clock.java
@@ -1,5 +1,13 @@
 package src.Item.Furniture;
 
 public class Clock extends Furniture {
-    
+    public Clock(Point lokasiClock){
+        super("Jam", lokasiClock, 1, 1, 10);
+    }
+
+    public void lihatWaktu(){
+        System.out.println(sim.getCurrentWorld().getClock().getRemainingTime());
+    }
+
 }
+
