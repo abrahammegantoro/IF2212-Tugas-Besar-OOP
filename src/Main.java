9ab8d1c66de1f53088c3d03791171b23f400630d
Gibran Fasha Ghazanfar <gibran.fsh@gmail.com>
2023-04-28 17:03:23 +0700

    banyak
    method masak udh bisa, nambahin beberapa method dan mengubah beberapa kode di class-class lain

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 package src;
 
 public class Main {
-    
+    // public static long timeNow = 0;
+
+    // public static void runTheTime() {
+    //     Thread temp = new Thread() {
+    //         public void run() {
+    //             while (true) {
+    //                 try {
+    //                     if (ada sim yang bekerja dan memakan waktu)
+    //                     Thread.sleep(1000);
+    //                     timeNow++;
+    //                 } catch (InterruptedException e) {
+    //                     e.printStackTrace();
+    //                 }
+    //             }
+    //         }
+    //     };
+    //     temp.start();
+    // }
 }
